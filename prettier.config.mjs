0a74e312c57d56d0b37f5648d199438201ea4bd8
Matt Venables <matt@venabl.es>
2025-06-14 08:50:37 -0400

    Create repo

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ *
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  plugins: ["prettier-plugin-packagejson"],
+}
+
+export default config
