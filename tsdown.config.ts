14a5a760546318a797ee0838ecdc5596b22eba70
Matt Venables <matt@venabl.es>
2025-06-14 13:21:30 -0400

    Simplify build and exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown"
 
 export default defineConfig({
   entry: "src/index.ts",
-  format: ["cjs", "esm"],
-  dts: true,
+  format: ["esm"],
+  dts: false,
   outDir: "dist"
 })
